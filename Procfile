f8ebb4701e41a8f9f6143f9bc7b8068f6f60289e
Vinicius Ronconi <vinicius.ronconi@gmail.com>
2017-10-24 16:33:16 -0200

    Add heroku config files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WeatherForecast.wsgi --log-file -
--- WeatherForecast/settings.py
+++ WeatherForecast/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(os.path.join(BASE_DIR, 'front-end'), 'build', 'static')
 ]
